03efcfc96d50e60e91e2ce5e2bbed54b3ceee875
tutkuince <tutku.ince@outlook.com>
2019-01-23 14:30:25 +0300

    Operators and Math Functions samples

---
+++ WebContent/js/app006.js
@@ -0,0 +1,46 @@
+let value;
+
+const val1 = 10;
+const val2 = 4;
+
+//
+// Arithmetic Operators
+//
+
+// value = val1 + val2;
+// value = val1 - val2;
+// value = val1 * val2;
+// value = val1 / val2;
+// value = val1 % val2;
+
+// value = Math.PI;
+value = Math.E;
+
+value = Math.round(3.6);    // 4
+value = Math.round(3.4);    // 3
+
+value = Math.ceil(3.2);     // 4
+value = Math.ceil(3.7);     // 7
+
+value = Math.floor(3.2);    // 3
+value = Math.floor(3.7);    // 3
+
+value = Math.sqrt(16);      // 4
+value = Math.sqrt(31);      // 5.56
+
+value = Math.abs(-10);      // 10
+
+value = Math.pow(2,3);  // 2 * 2 * 2
+value = Math.pow(4, 2); // 4 * 4
+
+value = Math.max(10, -1, 100, 43);  // 100
+value = Math.min(10, -1, 100, 43);  // -1
+
+value = Math.random();  // 0 to 1
+
+value = Math.random() * 20; // 0 to 20(exclusive)
+
+value = Math.floor(Math.random() * 20 + 1); // 1(inclusive) to 20(inclusive)
+
+
+console.log(value);

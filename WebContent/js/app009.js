de02cde4e9556ab21720e5fa1669f0a9b48b668c
tutkuince <tutku.ince@outlook.com>
2019-01-23 15:20:44 +0300

    Arrays

---
+++ WebContent/js/app009.js
@@ -0,0 +1,51 @@
+let value;
+
+// const numbers = [43, 56, 12, 44, 55];
+const numbers = [44, 35, 23, 6, 55];
+
+// const numbers2 = new Array(1, 2, 3, 4, 5);
+
+const langs = ["Java", "Python", "C++", "Javascript"];
+
+const a = ["Hello", 22, null, undefined, 3.14];
+
+//
+//  index
+//
+// value = numbers.length;
+// value = numbers[0];
+// value = numbers[numbers.length - 1];
+//
+// numbers[0] = 99;
+// value = numbers;
+//
+// value = numbers.indexOf(99);
+
+// numbers.push(100);  // [43, 56, 12, 44, 55, 100]
+//
+// numbers.unshift(99);    // [99, 43, 56, 12, 44, 55, 100]
+//
+// numbers.pop();  // [99, 43, 56, 12, 44, 55]
+//
+// numbers.shift();    // [43, 56, 12, 44, 55]
+//
+// numbers.splice(0, 3);   // [44, 55]
+//
+// numbers.reverse();  // [55, 44]
+
+// numbers.sort((a, b) => {return a - b;})
+// numbers.sort((a, b) => {return b - a;})
+
+// numbers.sort(function (a, b) {
+//     return a - b;
+// })
+
+// numbers.sort(function (a, b) {
+//     return b - 1;
+// })
+
+value = numbers;
+
+
+
+console.log(value);

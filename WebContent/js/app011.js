d4c324417a586179b0b3bbb6a16fc424d757aadc
tutkuince <tutku.ince@outlook.com>
2019-01-23 15:44:47 +0300

    Date Methods

---
+++ WebContent/js/app011.js
@@ -0,0 +1,30 @@
+let value;
+
+// const now = new Date();
+
+// value = now;
+
+const date1 = new Date("01-15-1989");
+// const date2 = new Date("January 01 1989");
+// const date3 = new Date("1/15/1989");
+
+// value = date1;
+// value = date2;
+// value = date3;
+
+// value = date1.getMonth();
+// value = date1.getDate();
+// value = date1.getFullYear();
+//
+// value = date1.getHours();
+// value = date1.getMinutes();
+// value = date1.getSeconds();
+// value = date1.getMilliseconds();
+
+date1.setMonth(10);
+date1.setDate(20);
+date1.setFullYear(1988);
+
+value = date1;
+
+console.log(value);

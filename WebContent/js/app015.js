e53a5d51c96df20a85f16718e78c1c60e974d229
tutkuince <tutku.ince@outlook.com>
2019-01-23 20:44:33 +0300

    Window Object

---
+++ WebContent/js/app015.js
@@ -0,0 +1,52 @@
+//
+// alert
+//
+// alert("Hi!");
+
+//
+//  confirm
+//
+// if(confirm("Are you sure?"))
+//     console.log("Deleted!");
+// else
+//     console.log("Canceled!");
+
+
+//
+//  prompt
+//
+// const answer = prompt("2 + 2 = ?");
+//
+// if (answer == "4")
+//     console.log("True");
+// else
+//     console.log("False");
+
+
+let value;
+
+// value = window;
+// value = window.location;
+// value = window.location.host;           // localhost:63342
+// value = window.location.hostname;   // localhost
+// value = window.location.port;       // 63342
+// value = window.location.href;
+
+
+//
+// if (confirm("Would you like to reload the page?"))
+//     window.location.reload();
+// else
+//     console.log("Page is not reloaded!");
+
+
+// value = window.outerHeight;
+// value = window.outerWidth;
+
+// value = window.innerHeight;
+// value = window.innerWidth;
+
+// value = window.scrollX;
+value = window.scrollY;
+
+console.log(value);

e6489e6cf0f0c4d61ba90bb35482f27ca8f95b2a
tutkuince <tutku.ince@outlook.com>
2019-01-23 12:54:50 +0300

    usage of console

---
+++ WebContent/js/app002.js
@@ -0,0 +1,12 @@
+console.log("Hello JS");
+console.log(111111);
+console.log(true);
+
+var a = 10;
+console.log('a: ' + a);
+
+console.log(["Tutku", "Emin"]);
+
+console.warn("This is a warning");
+console.error("This page is not found!");
+

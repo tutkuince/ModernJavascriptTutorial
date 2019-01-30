92cbfbbc68c6aa047a8ad12163948324d44e01a3
tutkuince <tutku.ince@outlook.com>
2019-01-30 18:07:49 +0300

    Arrow Function

---
+++ WebContent/js/app026.js
@@ -0,0 +1,34 @@
+// const sayHi = function () {
+//     console.log("Hello!");
+// }
+// sayHi();
+
+//
+// Arrow Function
+//
+// const sayHi = () => {
+//     console.log("Hello!");
+// }
+// sayHi();
+
+// const sayHi = (name) => {console.log(`Hello to ${name}`);}
+// sayHi("Tutku");
+
+// const sayHi = name => console.log(`Hello to ${name}`);
+// sayHi("Tutku");
+
+// const sayHi = (name, surname) => console.log("Hi", name, surname);
+// sayHi("Tutku", "Ince");
+
+
+// const cube = function (x) {
+//     return x * x * x;
+// }
+// console.log(cube(4));
+
+// const cube = x => x * x * x; // It works for 1 process
+
+const cube = x => {
+    return x * x * x;
+}
+console.log(cube(4));
